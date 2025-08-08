84ecaf72c17e82577b77b9cce5eca60a8815a86e
agripe049 <agripeheber@gmail.com>
2025-08-07 22:26:23 -0300

    Ajustes tecnicos na home, portfolio, especialidades

--- src/pages/Especialidades.js
+++ src/pages/Especialidades.js
@@ -7,7 +7,7 @@ import { IoLogoFirebase } from "react-icons/io5";
 
 function Contato() {
     return (
-      <div>
+      <div> 
         <section className={styles.especialidades}>
             <div className={styles.interface}>
               <h2 className={styles.titulo}>MINHAS <span>ESPECIALIDADES.</span></h2>
@@ -15,27 +15,27 @@ function Contato() {
                     <div className={styles.especialidades_box}>
                         <i><FaHtml5 /></i>
                         <h3>HTML</h3>
-                        <p>3 anos de experiência.</p>
+                        <p>3 anos de experiência</p>
                     </div>
                     <div className={styles.especialidades_box}>
                         <i><FaCss3Alt /></i>
                         <h3>CSS</h3>
-                        <p>3 anos de experiência.</p>
+                        <p>3 anos de experiência</p>
                     </div>
                     <div className={styles.especialidades_box}>
                         <i><IoLogoJavascript /></i>
                         <h3>JavaScript</h3>
-                        <p>3 anos de experiência.</p>
+                        <p>3 anos de experiência</p>
                     </div>
                     <div className={styles.especialidades_box}>
                         <i><FaReact /></i>
                         <h3>React</h3>
-                        <p>2 anos de experiência.</p>
+                        <p>2 anos de experiência</p>
                     </div>
                     <div className={styles.especialidades_box}>
                         <i><IoLogoFirebase /></i>
                         <h3>Firebase</h3>
-                        <p>2 anos de experiência.</p>
+                        <p>2 anos de experiência</p>
                     </div>
                 </div>
             </div>
