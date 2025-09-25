acb419be670645cc2495f299108fcb86d27751cd
agripe049 <agripeheber@gmail.com>
2025-09-25 12:42:47 -0300

    casamento

--- src/pages/GerenciamentoCasamento.js
+++ src/pages/GerenciamentoCasamento.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 import { FaArrowLeftLong } from "react-icons/fa6";
 import dash from '../img/dash.png'
 
-
+ 
 function GerenciamentoCasamento() {
     return (
         <div>
