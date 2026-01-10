0a7aa15ca47f800c491ec6f5984e71ba5b38b2fd
agripe049 <agripeheber@gmail.com>
2026-01-10 13:30:42 -0300

    Css de projetos pronto

--- src/pages/Projetos.js
+++ src/pages/Projetos.js
@@ -1,95 +1,105 @@
 import styles from './Projetos.module.css';
-import login from '../img/login.png';
 import { Link } from 'react-router-dom';
+import login from '../img/login.png';
 import dash from '../img/dash.png';
-import casamentoHome from "../img/casamentoHome.png";
+import casamentoHome from '../img/casamentoHome.png';
 
 function Projetos() {
   return (
-    <div>
-      <section className={styles.portfolio}>
-        <div className={styles.interface}>
-          <h2 className={styles.titulo}>MEUS <span>PROJETOS.</span></h2>
-          <div className={styles.flex}>
-            <div className={styles.img_portfolio}>
+    <section className={styles.portfolio}>
+      <div className={styles.interface}>
+        <h2 className={styles.titulo}>
+          MEUS <span>PROJETOS.</span>
+        </h2>
+
+        {/* POT CAKES */}
+        <div className={styles.project}>
+          <div className={styles.projectImg}>
+            <Link to="/potcakes">
+              <img src={login} alt="Pot Cakes" />
+            </Link>
+          </div>
+
+          <div className={styles.projectText}>
+            <h2>Pot Cakes Confeitaria</h2>
+            <p>Sistema desenvolvido para gerenciamento de confeitaria.</p>
+
+            <div className={styles.projectTech}>
+              <button>HTML</button>
+              <button>CSS</button>
+              <button>JavaScript</button>
+              <button>React</button>
+              <button>Firebase</button>
+            </div>
+
+            <div className={styles.btn_verprojeto}>
               <Link to="/potcakes">
-                <img src={login} alt="Login" className={styles.login} />
+                <p>Ver projeto</p>
               </Link>
-            </div>
-            <div className={styles.txt_portfolio}>
-              <h2>Pot Cakes Confeitaria</h2>
-              <p>Sistema desenvolvido para gerenciamento de confeitaria.</p>
-              <div className={styles.btn_portfolio}>
-                <button>HTML</button>
-                <button>CSS</button>
-                <button>JavaScript</button>
-                <button>React</button>
-                <button>Firebase</button>
-              </div>
-              <div className={styles.btn_verprojeto}>
-                <Link to="/potcakes">
-                  <p>Ver projeto</p>
-                </Link>
-              </div>
-            </div>
-          </div>
-
-
-          {/*----------------------------------------------------------------------------------------------------------------------*/}
-
-
-          <div className={styles.flex}>
-            <div className={styles.img_gerenciamento}>
-              <Link to="/gerenciamentoCasamento">
-                <img src={dash} alt="Dash" className={styles.login} />
-              </Link>
-            </div>
-            <div className={styles.txt_gerenciamento}>
-              <h2>Gerenciador de orçamentos</h2>
-              <p>Sistema desenvolvido para gerenciamento de orçamentos para casamentos</p>
-              <div className={styles.btn_gerenciamento}>
-                <button>HTML</button>
-                <button>CSS</button>
-                <button>JavaScript</button>
-                <button>React</button>
-                <button>Firebase</button>
-              </div>
-              <div className={styles.btn_verprojeto}>
-                <Link to="/gerenciamentoCasamento">
-                  <p>Ver projeto</p>
-                </Link>
-              </div>
-            </div>
-          </div>
-
-
-          {/*----------------------------------------------------------------------------------------------------------------------*/}
-          <div className={styles.flex}>
-            <div className={styles.img_convite}>
-              <Link to="/conviteCasamento">
-                <img src={casamentoHome} alt="Home" className={styles.login} />
-              </Link>
-            </div>
-            <div className={styles.txt_convite}>
-              <h2>Convite de casamento</h2>
-              <p>Sistema de convite de casamento</p>
-              <div className={styles.btn_convite}>
-                <button>HTML</button>
-                <button>CSS</button>
-                <button>JavaScript</button>
-                <button>React</button>
-                <button>Firebase</button>
-              </div> 
-               <div className={styles.btn_verprojeto}>
-                <Link to="/conviteCasamento">
-                  <p>Ver projeto</p>
-                </Link>
-              </div>
             </div>
           </div>
         </div>
-      </section>
-    </div>
+
+        {/* GERENCIAMENTO */}
+        <div className={styles.project}>
+          <div className={styles.projectImg}>
+            <Link to="/gerenciamentoCasamento">
+              <img src={dash} alt="Gerenciador de Orçamentos" />
+            </Link>
+          </div>
+
+          <div className={styles.projectText}>
+            <h2>Gerenciador de orçamentos</h2>
+            <p>
+              Sistema desenvolvido para gerenciamento de orçamentos para
+              casamentos.
+            </p>
+
+            <div className={styles.projectTech}>
+              <button>HTML</button>
+              <button>CSS</button>
+              <button>JavaScript</button>
+              <button>React</button>
+              <button>Firebase</button>
+            </div>
+
+            <div className={styles.btn_verprojeto}>
+              <Link to="/gerenciamentoCasamento">
+                <p>Ver projeto</p>
+              </Link>
+            </div>
+          </div>
+        </div>
+
+        {/* CONVITE */}
+        <div className={styles.project}>
+          <div className={styles.projectImg}>
+            <Link to="/conviteCasamento">
+              <img src={casamentoHome} alt="Convite de Casamento" />
+            </Link>
+          </div>
+
+          <div className={styles.projectText}>
+            <h2>Convite de casamento</h2>
+            <p>Sistema de convite de casamento.</p>
+
+            <div className={styles.projectTech}>
+              <button>HTML</button>
+              <button>CSS</button>
+              <button>JavaScript</button>
+              <button>React</button>
+              <button>Firebase</button>
+            </div>
+
+            <div className={styles.btn_verprojeto}>
+              <Link to="/conviteCasamento">
+                <p>Ver projeto</p>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
